616c2397e844781ef242c7a1f48b76ccc87c7095
GauravB <gauravb@GauravBs-MacBook-Air.local>
2026-08-11 22:29:58 +0530

    logging file added

---
+++ MultiAgentClinicalTrial-IntelligenceusingLangGraphLangMemGCP/mosaic/config/logging_config.py
@@ -0,0 +1,33 @@
+
+
+# Central Logging Setup for Entire Project 
+
+import logging 
+import sys
+
+def setup_logging(name: str) -> logging.Logger:
+    """_summary_
+
+    Args:
+        name (str): _description_
+
+    Returns:
+        logging.Logger: _description_
+    """
+    logger = logging.getLogger(name) 
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",        
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.propagate = False
+    return logger
